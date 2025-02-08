3cc94c928ed04c77992bba259f076d6c93a89730
amirhskh <amirho3einkhosravi123456789@gmail.com>
2025-02-08 13:54:43 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+ # print("hello world")
+
+
+# global 
+
+def main():
+
+
+
+    print("hello world");
+
+main()
+
+# local
+
+ # x = 10
+
+ # print(x)
+
+
+x = int = 11
+
+print(type(x))
+
+y = float = 12
+print(type(y))
+
+a = 12
+
+a = a + 1
+
+a += 1
+
+first_name = "amir" 
+
+last_name = "khosravi"
+
+print("welcom" + " " + first_name + " " + last_name )
+
+x = "amir"
+
+print(x.startswith("a"))
+
+a2 = 'mmmmmaaaaa'
+
+print(a2.count("m"))
